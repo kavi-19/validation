a0f583c612375fd4fbce180ac300a89a11470867
kavi-19 <kavithakaari1924@gmail.com>
2024-06-01 16:02:02 +0530

    initial commit

--- script.js
+++ script.js
@@ -7,19 +7,21 @@ var submitError = document.getElementById('submit-error')
 function validateName(){
     var name = document.getElementById('contact-name').value;
     if(name.length == 0){
-        nameError.innerHTML = 'Name is required';
-        return false;
+      nameError.innerHTML = 'Name is required';
+      return false;
 }
-nameError.innerHTML='<i class="fa-solid fa-check-to-slot"></i>';
-return true;
+
+  nameError.innerHTML ="valid"
+  return true;
 }
+
 function validatePhone(){
   var phone = document.getElementById('contact-phone').value;
   if(phone.length == 0){
     phoneError.innerHTML = 'Phone no is required';
     return false;  
   }
-  if(phone.length !== 10){
+  if(phone.length == 10){
     phoneError.innerHTML ='Phone no should be 10 digits'
     return false;
   }
@@ -30,9 +32,20 @@ function validateEmail(){
     phoneError.innerHTML = 'Email no is required';
     return false;
     }
-    emailError.innerHTML='<i class="fa-solid fa-check-to-slot"></i>';
+    emailError.innerHTML='valid';
     return true;
 
   }
+  function validateMessage(){
+    var message = document.getElementById('contact-message').value;
+    var required =30;
+    var left = required - message.length;
 
-}
+    if(left>0){
+      messageError.innerHTML = left + 'more characters required';
+      return false;
+    }
+
+   messageError.innerHTML='<i class="fa-solid fa-check-to-slot"></i>';
+    return true;
+  }
