1e89f948e3f3c22dfe28fceb745b2c8e2058b670
kavi-19 <kavithakaari1924@gmail.com>
2024-06-01 14:24:13 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+var nameError = document.getElementById('name-error');
+var phoneError = document.getElementById('phone-error')
+var emailError = document.getElementById('email-error')
+var messageError = document.getElementById('message-error')
+var submitError = document.getElementById('submit-error')
+
+function validateName(){
+    var name = document.getElementById('contact-name').value;
+    if(name.length == 0){
+        nameError.innerHTML = 'Name is required';
+        return false;
+}
+nameError.innerHTML='<i class="fa-solid fa-check-to-slot"></i>';
+return true;
+}
+function validatePhone(){
+  var phone = document.getElementById('contact-phone').value;
+  if(phone.length == 0){
+    phoneError.innerHTML = 'Phone no is required';
+    return false;  
+  }
+  if(phone.length !== 10){
+    phoneError.innerHTML ='Phone no should be 10 digits'
+    return false;
+  }
+ }
+function validateEmail(){
+  var email = document.getElementById('contact-email').value;
+  if(email.length == 0){
+    phoneError.innerHTML = 'Email no is required';
+    return false;
+    }
+    emailError.innerHTML='<i class="fa-solid fa-check-to-slot"></i>';
+    return true;
+
+  }
+
+}
